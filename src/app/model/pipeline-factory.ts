1a386c9e47553e2861840b942b71c724d064e24c
Daniele Bordignon <daniele.bordignon@koerber.com>
2025-10-23 00:09:59 +0200

    Working offline

--- src/app/model/pipeline-factory.ts
+++ src/app/model/pipeline-factory.ts
@@ -1,4 +1,12 @@
-import { pipeline } from '@huggingface/transformers';
+import { env, pipeline } from '@huggingface/transformers';
+
+env.allowLocalModels = true; // ✅ allow local files
+env.localModelPath = 'assets/models'; // ✅ base path for models
+
+env.backends.onnx!.wasm!.wasmPaths = {
+  wasm: 'assets/wasm/ort-wasm-simd-threaded.jsep.wasm',
+};
+
 // Define model factories
 // Ensures only one model is created of each type
 export class PipelineFactory {
@@ -7,14 +15,16 @@ export class PipelineFactory {
   static instance: any = null;
 
   static async getInstance(progressCallback?: (data: any) => void) {
-    if (this.instance === null && this.model) {
-      this.instance = pipeline(this.task, this.model, {
+    const localModelPath = 'whisper-small';
+    if (this.instance === null && localModelPath) {
+      this.instance = pipeline(this.task, localModelPath, {
         dtype: {
-          encoder_model: this.model === 'onnx-community/whisper-large-v3-turbo' ? 'fp16' : 'fp32',
+          encoder_model: 'fp32',
           decoder_model_merged: 'q4', // or 'fp32' ('fp16' is broken)
         },
         device: 'webgpu',
         progress_callback: progressCallback,
+        local_files_only: true,
       });
     }
 
--- src/app/storage/transcriber-config.storage.ts
+++ src/app/storage/transcriber-config.storage.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranscriberConfigStorage {
   samplingRate = 16000;
-  model = 'onnx-community/whisper-base';
+  model = 'onnx-community/whisper-small';
   subtask = 'transcribe';
   language = 'english';
   isMultilingual = true;
 
-  constructor(private deviceDetectorService: DeviceDetectorService) {
-    // isMobileOrTablet
-    this.model = this.deviceDetectorService.isDesktop() ? 'onnx-community/whisper-base' : 'onnx-community/whisper-tiny';
+  constructor() {
+    this.model = 'onnx-community/whisper-small';
   }
 }
